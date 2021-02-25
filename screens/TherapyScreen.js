8bd79dcb469fa443f93fb4d6d3c6a7ac546ceead
Shehzad Haleem <shehzad.haleem@kcl.ac.uk>
2021-02-25 15:25:58 +0000

    fixed JSON parsing error

--- screens/TherapyScreen.js
+++ screens/TherapyScreen.js
@@ -1,45 +1,48 @@
-import React from 'react';
-import { StyleSheet, SafeAreaView, Text, View, TouchableOpacity } from 'react-native';
-import Constants from 'expo-constants';
+import React from "react";
+import {
+  StyleSheet,
+  SafeAreaView,
+  Text,
+  View,
+  TouchableOpacity,
+} from "react-native";
+import Constants from "expo-constants";
 
 const TherapyScreen = () => {
   return (
     <SafeAreaView style={styles.container}>
-
       <TouchableOpacity>
         <SafeAreaView style={styles.takebreak}>
           <Text style={styles.text}>Take a break</Text>
-       </SafeAreaView>
-        </TouchableOpacity>
+        </SafeAreaView>
+      </TouchableOpacity>
 
-        <View style={styles.area}>
-          <View style={styles.inner_area}>
-              <Text style={styles.box_text}>Question here</Text>
-          </View>
+      <View style={styles.area}>
+        <View style={styles.inner_area}>
+          <Text style={styles.box_text}>Question here</Text>
+        </View>
 
-          <TouchableOpacity>
-            <SafeAreaView style={styles.yes}>
-              <Text style={styles.text}>YES</Text>
+        <TouchableOpacity>
+          <SafeAreaView style={styles.yes}>
+            <Text style={styles.text}>YES</Text>
           </SafeAreaView>
         </TouchableOpacity>
 
         <TouchableOpacity>
-            <SafeAreaView style={styles.no}>
-              <Text style={styles.text}>NO</Text>
+          <SafeAreaView style={styles.no}>
+            <Text style={styles.text}>NO</Text>
           </SafeAreaView>
         </TouchableOpacity>
-
-        </View>
+      </View>
 
       <TouchableOpacity>
-       <SafeAreaView style={styles.next}>
+        <SafeAreaView style={styles.next}>
           <Text style={styles.text}>Next</Text>
-       </SafeAreaView>
+        </SafeAreaView>
       </TouchableOpacity>
-    
     </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -53,8 +56,8 @@ const styles = StyleSheet.create({
     marginHorizontal: 150,
     height: 40,
     backgroundColor: "#94ffd3",
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     borderRadius: 50,
   },
 
@@ -74,30 +77,30 @@ const styles = StyleSheet.create({
     marginHorizontal: 150,
     height: 40,
     backgroundColor: "#94ffd3",
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     borderRadius: 50,
   },
 
   area: {
     marginHorizontal: 20,
-    width: '90%',
-    height: '75%',
-    padding: '5',
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    width: "90%",
+    height: "75%",
+    padding: 5,
+    backgroundColor: "#fff",
+    alignItems: "center",
     borderRadius: 25,
   },
 
   inner_area: {
     marginHorizontal: 50,
-    width: '85%',
-    height: '65%',
-    padding: '5',
+    width: "85%",
+    height: "65%",
+    padding: 5,
     backgroundColor: "#f4f0eb",
-    alignItems: 'center',
+    alignItems: "center",
     borderRadius: 25,
-    position: 'absolute',
+    position: "absolute",
     left: -20,
     top: 20,
   },
@@ -106,10 +109,10 @@ const styles = StyleSheet.create({
     width: 200,
     height: 40,
     backgroundColor: "#ffd0c1",
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     borderRadius: 8,
-    position: 'absolute',
+    position: "absolute",
     right: -100,
     top: 450,
   },
@@ -118,10 +121,10 @@ const styles = StyleSheet.create({
     width: 200,
     height: 40,
     backgroundColor: "#ffd0c1",
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     borderRadius: 8,
-    position: 'absolute',
+    position: "absolute",
     right: -100,
     top: 500,
   },
