9828915ff5dbd35348a8a0e4574b3ece5c3483c3
Julianne Diaz <julianne.diaz@kcl.ac.uk>
2021-02-27 23:02:08 +0000

    Add Category Drop Screen

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import TherapyScreen from './screens/TherapyScreen';
+import CategoryDrop from './screens/CategoryDrop';
 
 export default class App extends React.Component {
   render() {
     return(
-      <TherapyScreen/>
+      //Uncomment to see different screens
+      //<TherapyScreen/> 
+      <CategoryDrop/>     
     )
   }
 }
