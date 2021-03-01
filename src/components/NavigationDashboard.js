546e99162270549c180f193f85998c478e12d845
Hiro Funatsuka <hiro.funatsuka@kcl.ac.uk>
2021-03-01 17:54:20 +0900

    Implement a bottom tab bar

---
+++ src/components/NavigationDashboard.js
@@ -0,0 +1,125 @@
+import * as React from 'react';
+import { View, Button, Text } from 'react-native';
+import {
+    NavigationContainer,
+    getFocusedRouteNameFromRoute,
+} from '@react-navigation/native';
+import { Ionicons } from '@expo/vector-icons';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
+
+function getHeaderTitle(route) {
+    // If the focused route is not found, we need to assume it's the initial screen
+    // This can happen during if there hasn't been any navigation inside the screen
+    // In our case, it's "Home" as that's the first screen inside the navigator
+    const routeName = getFocusedRouteNameFromRoute(route) ?? 'Home';
+
+    switch (routeName) {
+        case 'Home':
+            return 'Home';
+        case 'Calendar':
+            return 'Calendar';
+        case 'Help':
+            return 'Help';
+        case 'Account':
+            return 'Account';
+    }
+}
+
+
+//Temporary screens to check the tab bar is working.
+function HomeScreen({ navigation }) {
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Button
+                title="Go to Sessions"
+                onPress={() => navigation.navigate('Sessions')}
+            />
+        </View>
+    );
+}
+
+function CalendarScreen() {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>Calendar!</Text>
+        </View>
+    );
+}
+
+function HelpScreen() {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>Help!</Text>
+        </View>
+    );
+}
+
+function AccountScreen() {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>Account!</Text>
+        </View>
+    );
+}
+
+function SessionsScreen() {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>Sessions!</Text>
+        </View>
+    );
+}
+
+const Tab = createBottomTabNavigator();
+
+function HomeTabs({ navigation, route }) {
+    React.useLayoutEffect(() => {
+        navigation.setOptions({ headerTitle: getHeaderTitle(route) });
+    }, [navigation, route]);
+
+    return (
+        <Tab.Navigator
+            screenOptions={({ route }) => ({
+                tabBarIcon: ({ focused, color, size }) => {
+                    let iconName;
+
+                    if (route.name === 'Home') {
+                        iconName = focused ? 'ios-home' : 'ios-home-outline';
+                    } else if (route.name === 'Calendar') {
+                        iconName = focused ? 'ios-calendar' : 'ios-calendar-outline';
+                    } else if (route.name === 'Help') {
+                        iconName = focused ? 'ios-chatbox-ellipses' : 'ios-chatbox-ellipses-outline';
+                    } else if (route.name === 'Account') {
+                        iconName = focused ? 'ios-person' : 'ios-person-outline';
+                    }
+
+                    // You can return any component that you like here!
+                    return <Ionicons name={iconName} size={size} color={color} />;
+                },
+            })}
+            tabBarOptions={{
+                activeTintColor: 'tomato',
+                inactiveTintColor: 'gray',
+            }}
+        >
+            <Tab.Screen name="Home" component={HomeScreen} />
+            <Tab.Screen name="Calendar" component={CalendarScreen} />
+            <Tab.Screen name="Help" component={HelpScreen} />
+            <Tab.Screen name="Account" component={AccountScreen} />
+        </Tab.Navigator>
+    );
+}
+
+const Stack = createStackNavigator();
+
+export default function NavigationDashboard() {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="Home" component={HomeTabs} />
+                <Stack.Screen name="Sessions" component={SessionsScreen} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+}
