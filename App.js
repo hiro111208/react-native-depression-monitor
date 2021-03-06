6d7cfb48a3f9e996feeba19203099fa639df5afd
Shehzad Haleem <shehzad.haleem@kcl.ac.uk>
2021-03-06 09:21:54 +0000

    fix text not displaying correctly in TherapyScreen.js

--- App.js
+++ App.js
@@ -1,17 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import TherapyScreen from './screens/TherapyScreen';
-import CategoryDrop from './screens/CategoryDrop';
-import PlantScreen from './screens/PlantScreen';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import TherapyScreen from "./screens/TherapyScreen";
+import CategoryDrop from "./screens/CategoryDrop";
+import PlantScreen from "./screens/PlantScreen";
 
 export default class App extends React.Component {
   render() {
-    return(
-      //Uncomment to see different screens
-      //<TherapyScreen/> 
+    return (
+      //Uncomment to see different screens one at a time
+      <TherapyScreen />
       //<CategoryDrop/>
-      <PlantScreen/>     
-    )
+      //<PlantScreen/>
+    );
   }
 }
-
--- screens/TherapyScreen.js
+++ screens/TherapyScreen.js
@@ -1,59 +1,52 @@
-import React from 'react';
-import { StatusBar, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
-import Constants from 'expo-constants'; 
+import React from "react";
+import {
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+} from "react-native";
+import Constants from "expo-constants";
 
 const TherapyScreen = () => {
-    return (
-      <View style={styles.container}>
-
-        <View style={[styles.top, styles.centering]}>
-          <TouchableOpacity style={[styles.optButton, styles.centering]}>      
-                <Text style={styles.text}>Take a break</Text>             
-          </TouchableOpacity>
-        </View>
-
-        <View style={[styles.center, styles.centering]}>
-          <View style={styles.question}>
-            <Text style={styles.text}>Question here</Text>
-          </View>
-        </View>
-
-        <View style={[styles.answers, styles.centering]}>
-          <TouchableOpacity style={[styles.ansButton, styles.centering]}>
-              <Text style={styles.text}>YES</Text>
-          </TouchableOpacity>
-
-          <TouchableOpacity style={[styles.ansButton, styles.centering]}>
-              <Text style={styles.text}>NO</Text>
-          </TouchableOpacity>
-        </View>
-
-        <View style={[styles.bottom, styles.centering]}>
-          <TouchableOpacity style={[styles.optButton, styles.centering]}>
-              <Text style={styles.text}>Next</Text>           
-          </TouchableOpacity>
-        </View>
-
+  return (
+    <View style={styles.container}>
+      <View style={[styles.top, styles.centering]}>
+        <TouchableOpacity style={[styles.optButton, styles.centering]}>
+          <Text style={styles.text}>Take a break</Text>
+        </TouchableOpacity>
       </View>
-    )
-}
+
+      <View style={[styles.center, styles.centering]}>
+        <View style={styles.question}>
+          <Text style={styles.text}>Question here</Text>
+        </View>
+      </View>
+
+      <View style={[styles.answers, styles.centering]}>
+        <TouchableOpacity style={[styles.ansButton, styles.centering]}>
+          <Text style={styles.text}>YES</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={[styles.ansButton, styles.centering]}>
+          <Text style={styles.text}>NO</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={[styles.bottom, styles.centering]}>
+        <TouchableOpacity style={[styles.optButton, styles.centering]}>
+          <Text style={styles.text}>Next</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-  container: {
-    marginTop: Constants.statusBarHeight,
-    flex: 1,
-    backgroundColor: '#ffd390'
-  },
-  optButton: {
-    height: '70%',
-    width: 140,
-    backgroundColor: '#c7ffd8',
-    borderRadius: 10,
-  },
   ansButton: {
-    height: '75%',
+    height: "75%",
     width: 250,
-    backgroundColor: '#ffd0c1',
+    backgroundColor: "#ffd0c1",
     borderRadius: 10,
     shadowColor: "#000",
     shadowOffset: {
@@ -65,37 +58,48 @@ const styles = StyleSheet.create({
     elevation: 5,
     marginVertical: 5,
   },
-  text: {
-    color: "black",
-    fontSize: 18,
-    marginVertical: 20,
-  },
-  top: {
-    height: '10%',
-  },
-  center: {
-    height: '50%',
-  },
-  question: {
-    width: '80%',
-    height: '100%',
-    alignItems: 'center',
-    borderRadius: 8,
-    borderWidth: 4,
-    borderColor: '#fff',
-    backgroundColor: '#eee'
-  },
   answers: {
-    height: '30%',
+    height: "30%",
     padding: 70,
   },
   bottom: {
-    height: '10%',
+    height: "10%",
+  },
+  center: {
+    height: "50%",
   },
   centering: {
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-})
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  container: {
+    marginTop: Constants.statusBarHeight,
+    flex: 1,
+    backgroundColor: "#ffd390",
+  },
+  optButton: {
+    height: "70%",
+    width: 140,
+    backgroundColor: "#c7ffd8",
+    borderRadius: 10,
+  },
+  question: {
+    width: "80%",
+    height: "100%",
+    alignItems: "center",
+    borderRadius: 8,
+    borderWidth: 4,
+    borderColor: "#fff",
+    backgroundColor: "#eee",
+  },
+  text: {
+    color: "black",
+    fontSize: 18,
+    //marginVertical: 20,
+  },
+  top: {
+    height: "10%",
+  },
+});
 
 export default TherapyScreen;
