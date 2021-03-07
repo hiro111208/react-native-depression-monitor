ffd2dc290e8cc5473220b88d26fdfd953c9b389c
madamcrystal <K19001568@kcl.ac.uk>
2021-03-07 01:15:14 +0000

    Add 'app' folder

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import PauseScreen from './screens/PauseScreen';
+import PauseScreen from './app/screens/PauseScreen';
 
 export default class extends React.Component {
   render(){
