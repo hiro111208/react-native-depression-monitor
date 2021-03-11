fe8973e13dcd40ec9083dd156de1fb69fedb5f5b
Alexander <armero.alex@gmail.com>
2021-03-11 09:47:05 +0000

    Resolving Testing issues

--- babel.config.js
+++ babel.config.js
@@ -1,16 +1,6 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: [
-      "babel-preset-expo",
-      "@babel/preset-env",
-      { targets: { node: "current" } },
-    ],
-    plugins: [
-      "@babel/plugin-transform-runtime",
-      "@babel/plugin-transform-async-to-generator",
-      "@babel/plugin-proposal-export-default-from",
-      "@babel/plugin-proposal-export-namespace-from",
-    ],
+    presets: ["babel-preset-expo"],
   };
 };
