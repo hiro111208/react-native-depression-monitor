43b6a928e83170e637872ea6eabb78491ca32a89
Alexander <armero.alex@gmail.com>
2021-03-11 09:08:38 +0000

    Testing Progress Bar, Updating gitignore

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,11 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
+    plugins: [
+      "@babel/plugin-transform-runtime",
+      "@babel/plugin-transform-async-to-generator",
+      "@babel/plugin-proposal-export-default-from",
+      "@babel/plugin-proposal-export-namespace-from",
+    ],
   };
 };
