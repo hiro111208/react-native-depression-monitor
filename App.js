e729bdb78007b021dbcaff9557d2ce22df6d392e
madamcrystal <K19001568@kcl.ac.uk>
2021-03-12 13:18:36 +0000

    Fix file paths

--- App.js
+++ App.js
@@ -1,17 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import PauseScreen from './app/screens/PauseScreen';
+import StatusBar from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Component, View } from "react-native";
+import PauseScreen from "./app/screens/PauseScreen";
 
-export default class extends React.Component {
-  render(){
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}}
+export default class App extends React.Component {
+  render() {
+    return (
+      //Uncomment to see different screens one at a time
+      <View style={styles.container}>
+        <PauseScreen />
+      </View>
+      //<CategoryDrop/>
+      //<PlantScreen/>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   container: {
--- app/screens/PauseScreen.js
+++ app/screens/PauseScreen.js
@@ -1,3 +1,13 @@
-import React from 'React';
-import { StatusBar, StyleSheet, Text } from 'react-native';
+import React, { Component } from 'react';
+import { Image, View, StyleSheet} from 'react-native';
 
+const PauseScreen = () => {
+    return (
+            <Image 
+                 style={{ width: 300, height: 300 }}
+                 resizeMode="contain"
+                 source={require('../assets/pause.png')}
+            />
+    )
+}
+export default PauseScreen;
