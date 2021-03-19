c8b8208e0c188a8869c04fb44f3abe81aa274e52
madamcrystal <K19001568@kcl.ac.uk>
2021-03-19 13:35:53 +0000

    Replace apostrophes

--- app/tests/PauseScreen.test.js
+++ app/tests/PauseScreen.test.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-import PauseScreen from '../screens/PauseScreen';
+import React from "react";
+import renderer from "react-test-renderer";
+import PauseScreen from "../screens/PauseScreen";
 
-test('PauseScreen renders correctly', () => {
+test("PauseScreen renders correctly", () => {
     const tree = renderer.create(<PauseScreen />).toJSON();
     expect(tree).toMatchSnapshot();
 });
