beb7a09959da40e12c80a89d33848f01f1cb9b1e
Julianne Diaz <julianne.diaz@kcl.ac.uk>
2021-03-25 22:50:41 +0000

    Initialise AdminHomeScreen

--- app/dashboards/AdminHomeScreen.js
+++ app/dashboards/AdminHomeScreen.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
-import { StyleSheet, View, Text, Button } from "react-native";
+import { StyleSheet, View, Text, Button, Image } from "react-native";
 import firebase from "../database/firebase";
 
 import colors from "../config/colors";
+import { TouchableOpacity } from "react-native";
 
 export default function AdminHomeScreen({ props, navigation }) {
   const [errorMessage, setErrorMessage] = useState("");
@@ -19,7 +20,36 @@ export default function AdminHomeScreen({ props, navigation }) {
   };
 
   return (
-    <View style={styles.container}>
+    <View style={[styles.container]}>
+      <View style={[styles.center, styles.shadowEffect, styles.cover]}>
+        <View style={{ height: "10%" }}></View>
+
+        <View style={[{ height: "40%" }, styles.centering]}>
+          <Text style={styles.helloFont}>Hello, admin!</Text>
+          <Image
+            style={{ width: 200, height: 200 }}
+            resizeMode="contain"
+            source={require("../assets/hand-logo.png")}
+          />
+        </View>
+
+        <View style={{ height: "15%" }}>
+          <TouchableOpacity
+            onPress={() => navigation.navigate("TherapyQuestionScreen")}
+            style={[styles.centering, styles.optButton, styles.cover]}
+          >
+            <Text style={[styles.optFont, styles.centering]}>
+              Therapy Question Management
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+{
+  /* <View style={styles.container}>
       <Text style={styles.textStyle}>Hello, Admin</Text>
       <Button
         color={colors.darkBorder}
@@ -31,21 +61,52 @@ export default function AdminHomeScreen({ props, navigation }) {
         title="Logout"
         onPress={() => signOut()}
       />
-    </View>
-  );
+    </View> */
 }
 
 const styles = StyleSheet.create({
+  center: {
+    backgroundColor: "#fed8b1",
+    borderRadius: 50,
+    borderWidth: 5,
+    borderColor: "#ffeed2",
+  },
+  centering: {
+    alignItems: "center",
+    justifyContent: "center",
+  },
   container: {
     flex: 1,
     display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    padding: 35,
+    padding: 25,
     backgroundColor: "#fff",
   },
-  textStyle: {
-    fontSize: 15,
-    marginBottom: 20,
+  cover: {
+    height: "100%",
+    width: "100%",
+  },
+  helloFont: {
+    fontSize: 25,
+    fontWeight: "bold",
+    color: "dimgray",
+  },
+  optFont: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "dimgray",
+  },
+  optButton: {
+    backgroundColor: "#ffeed2",
+  },
+  shadowEffect: {
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+    marginVertical: 5,
   },
 });
