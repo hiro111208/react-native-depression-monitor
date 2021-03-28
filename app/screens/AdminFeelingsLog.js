63016f1f7399a4f4cfc6a54476cf7d1737b0703d
Shehzad Haleem <shehzad.haleem@kcl.ac.uk>
2021-03-28 05:38:40 +0100

    add AdminFeelingsLog.js to app

---
+++ app/screens/AdminFeelingsLog.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { SafeAreaView, Text } from "react-native";
+
+const AdminFeelingsLog = () => {
+  return (
+    <SafeAreaView>
+      <Text> Test </Text>
+    </SafeAreaView>
+  );
+};
+
+export default AdminFeelingsLog;
