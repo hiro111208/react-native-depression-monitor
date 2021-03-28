a1b1642269f45e0d01f5b9b3d19fe2882df3f330
Shehzad Haleem <shehzad.haleem@kcl.ac.uk>
2021-03-28 05:53:40 +0100

    add AdminFeelingsLog.js to HomeScreen

--- app/screens/AdminFeelingsLog.js
+++ app/screens/AdminFeelingsLog.js
@@ -1,12 +1,33 @@
 import React from "react";
-import { SafeAreaView, Text } from "react-native";
+import { ScrollView } from "react-native";
+import { SafeAreaView, View, Text, StyleSheet } from "react-native";
 
 const AdminFeelingsLog = () => {
   return (
-    <SafeAreaView>
-      <Text> Test </Text>
+    <SafeAreaView style={styles.container}>
+      <ScrollView style={styles.data}>{/* {renderData()} */}</ScrollView>
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#ffbe7bff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  data: {
+    width: "100%",
+    padding: 5,
+  },
+  oval: {
+    width: "100%",
+    height: 50,
+    borderRadius: 50,
+    backgroundColor: "white",
+    alignSelf: "center",
+  },
+});
 
 export default AdminFeelingsLog;
