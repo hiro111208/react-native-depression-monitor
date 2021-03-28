ab65442d7784386ca5b35ebbd75ab6fc4897006a
Shehzad Haleem <shehzad.haleem@kcl.ac.uk>
2021-03-28 22:22:22 +0100

    create switch case method to convert score in feelings log

--- app/screens/AdminFeelingsLog.js
+++ app/screens/AdminFeelingsLog.js
@@ -59,6 +59,20 @@ const AdminFeelingsLog = () => {
     );
   }
 
+  function switchScore(score) {
+    switch (score) {
+      case 1:
+        return 5;
+      case 2:
+        return 4;
+      case 3:
+        return 3;
+      case 4:
+        return 2;
+      case 5:
+        return 1;
+    }
+
   //Render the data of the feelings log
   if (loading) {
     return (
