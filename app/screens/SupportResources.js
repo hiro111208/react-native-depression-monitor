fa2b81bf9d38504798fab001442ffe51ce2a1f5f
Julianne Diaz <julianne.diaz@kcl.ac.uk>
2021-03-29 20:00:32 +0100

    Add scrollview horizontal

--- app/screens/SupportResources.js
+++ app/screens/SupportResources.js
@@ -14,7 +14,14 @@ import { AntDesign } from "@expo/vector-icons";
 const SupportResources = ({ navigation }) => {
   return (
     <View style={styles.container}>
-      <View style={[styles.center, styles.cover, styles.shadowEffect]}>
+      <View
+        style={[
+          styles.center,
+          styles.cover,
+          styles.shadowEffect,
+          styles.centering,
+        ]}
+      >
         <View style={[{ height: "10%" }, styles.centering]}>
           <Text style={[styles.fontStyle, { fontSize: 22 }]}>
             Support Resources
@@ -22,19 +29,17 @@ const SupportResources = ({ navigation }) => {
         </View>
 
         <ScrollView
-          style={[
-            {
-              height: "80%",
-              paddingLeft: 30,
-              paddingRight: 30,
-              paddingTop: 20,
-            },
-          ]}
+          style={{
+            height: "81%",
+          }}
         >
           <Text
             style={[
               styles.centering,
               {
+                paddingLeft: 30,
+                paddingRight: 30,
+                paddingTop: 20,
                 fontSize: 17,
                 fontStyle: "italic",
                 color: "dimgray",
@@ -44,21 +49,117 @@ const SupportResources = ({ navigation }) => {
             Asking for help doesn't make you weak - it reveals strength even
             when you don't feel strong.
           </Text>
+          <View style={{ height: "10%" }}></View>
+          <View style={{ height: "10%", backgroundColor: "#ffeed2" }}></View>
+
+          <View style={{ height: "78%", backgroundColor: "#ffeed2" }}>
+            <ScrollView horizontal>
+              <View style={[styles.boxSmall, styles.boxMargin]}>
+                <Text style={styles.title}>NHS</Text>
+                <Text
+                  style={styles.hyperlinkStyle}
+                  onPress={() => {
+                    Linking.openURL(
+                      "https://www.nhs.uk/service-search/mental-health/find-an-urgent-mental-health-helpline"
+                    );
+                  }}
+                >
+                  NHS Helplines website
+                </Text>
+                <Text>Local NHS urgent mental health helplines</Text>
+                <View style={styles.centering}></View>
+              </View>
+
+              <View style={[styles.boxSmall, styles.boxMargin]}>
+                <Text style={styles.title}>Bipolar UK:</Text>
+                <Text
+                  style={styles.hyperlinkStyle}
+                  onPress={() => {
+                    Linking.openURL("www.bipolaruk.org");
+                  }}
+                >
+                  www.bipolaruk.org
+                </Text>
+                <Text />
+                <Text>
+                  Supply a range of information leaflets, books and tapes.
+                  Network of self help groups for people with manic depression,
+                  relatives and friends.
+                </Text>
+              </View>
+
+              <View style={[styles.boxSmall, styles.boxMargin]}>
+                <Text style={styles.title}>Calmzone:</Text>
+                <Text
+                  style={styles.hyperlinkStyle}
+                  onPress={() => {
+                    Linking.openURL("www.thecalmzone.net");
+                  }}
+                >
+                  www.thecalmzone.net
+                </Text>
+                <Text />
+                <Text>
+                  Campaign Against Living Miserably. Help and support for young
+                  men aged 15-35 on issues which include depression and suicide.
+                </Text>
+              </View>
+
+              <View style={[styles.boxSmall, styles.boxMargin]}>
+                <Text style={styles.title}>Samaritans:</Text>
+                <Text
+                  style={styles.hyperlinkStyle}
+                  onPress={() => {
+                    Linking.openURL(
+                      "https://www.samaritans.org/how-we-can-help/schools/deal/deal-resources/dealing-feelings/what-is-depression/?gclid=Cj0KCQiAnKeCBhDPARIsAFDTLTIkkHjz1hc7XNw6RtOqswMgB1zpQlKHWeRA5uEqnai06Qw63RGoqQsaAlbJEALw_wcB"
+                    );
+                  }}
+                >
+                  Talk to a Samaritan
+                </Text>
+                <Text></Text>
+                <Text>
+                  Learn more about depression and how to detect it or contact a
+                  Samaritan to have a chat
+                </Text>
+              </View>
+
+              <View style={[styles.boxSmall, styles.boxMargin]}>
+                <Text style={styles.title}>WLM:</Text>
+                <Text>highburycounselling@wlm.org.uk</Text>
+                <Text>
+                  WLM Highbury Counselling Centre offers counselling and
+                  sychotherapy service for adults, a space to speak with a
+                  trained professional.
+                </Text>
+              </View>
+            </ScrollView>
+          </View>
+          <View style={{ height: "4%", backgroundColor: "#ffeed2" }}></View>
+          <View style={{ height: "3%" }}></View>
         </ScrollView>
 
-        <View style={[{ height: "10%" }, styles.centering]}>
+        <View
+          style={[
+            { height: "9%", width: "40%" },
+            styles.centering,
+            styles.shadowEffect,
+          ]}
+        >
           <TouchableOpacity
             onPress={() => navigation.goBack()}
             style={[
               styles.optButton,
               styles.cover,
               styles.centering,
-              { borderBottomLeftRadius: 40, borderBottomRightRadius: 40 },
+              { borderRadius: 50 },
             ]}
           >
             <Text style={[styles.fontStyle, { fontSize: 20 }]}>Return</Text>
           </TouchableOpacity>
         </View>
+
+        <View style={{ height: "1%" }}></View>
       </View>
     </View>
   );
@@ -101,7 +202,7 @@ const SupportResources = ({ navigation }) => {
 
         <ScrollView horizontal>
           <View style={[styles.boxSmall, styles.boxMargin]}>
-            <Text style={styles.title}>NHS:</Text>
+            <Text style={styles.title}>NHS</Text>
             <Text
               style={styles.hyperlinkStyle}
               onPress={() => {
@@ -264,6 +365,55 @@ const styles = StyleSheet.create({
   },
   textStyle: {
     fontSize: 18,
+  },
+  boxSmall: {
+    marginLeft: 10,
+    width: 225,
+    height: 180,
+    backgroundColor: "skyblue",
+    borderRadius: 40,
+  },
+  boxLarge: {
+    width: 380,
+    height: 350,
+    backgroundColor: "#ffa351",
+  },
+  boxMargin: {
+    marginRight: 10,
+    padding: 15,
+  },
+  bigWhite: {
+    margin: 15,
+    fontSize: 25,
+    fontWeight: "bold",
+    color: "white",
+  },
+  hyperlinkStyle: {
+    color: "blue",
+  },
+  logo: {
+    width: 400,
+    height: 100,
+  },
+  main: {
+    color: "white",
+    textAlign: "center",
+    fontSize: 17,
+  },
+  tinyLogo: {
+    width: 160,
+    height: 80,
+    margin: 10,
+  },
+  title: {
+    color: "white",
+    fontSize: 23,
+    fontWeight: "bold",
+  },
+  smallTitle: {
+    color: "white",
+    fontSize: 15,
+    margin: 2,
   },
 });
 
