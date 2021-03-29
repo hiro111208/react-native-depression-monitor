081b00359299c97d4710db31d64701d27fc13e94
k1921917 <k1921917@kcl.ac.uk>
2021-03-29 17:31:49 +0100

    Use gifs instead of images

--- app/screens/DemoScreen.js
+++ app/screens/DemoScreen.js
@@ -15,7 +15,7 @@ function DemoScreen({ navigation, route }){
   //depending on whether it is the user's first time using the app
   const navigate=()=>{
     if (params && params.user.categoryDropped == "NONE") {
-      navigation.navigate("CategoryDrop", {user: user})
+      navigation.navigate("CategoryDrop", {user: params.user})
     }
     else{
       navigation.navigate("PatientDashboard")
@@ -35,32 +35,32 @@ return(
     pages={[
       {
         backgroundColor: '#fff',
-        image: <Image resizeMode={'contain'} style={styles.image} source={require('../assets/demo/CategoryDrop.png')} />,
+        image: <Image resizeMode={'contain'} style={styles.image} source={require('../assets/demo/CategoryDropGif.gif')} />,
         title: 'Drop a category',
         subtitle: 'Select the category you are least interested in to be dropped from the therapy questions',
         imageContainerStyles:{padding:30},
       },
       {
         backgroundColor: '#fff',
-        image: <Image resizeMode={'contain'} style={styles.image} source={require('../assets/demo/Scheduler.png')} />,
+        image: <Image resizeMode={'contain'} style={styles.image} source={require("../assets/demo/SchedulerGif.gif")} />,
         title: 'Schedule your therapy sessions',
         subtitle: 'Select a date and time for your therapy sessions to be reminded using notifications',
       },
       {
         backgroundColor: '#fff',
-        image: <Image resizeMode={'contain'} style={styles.image} source={require('../assets/demo/TherapyScreen.png')} />,
+        image: <Image resizeMode={'contain'} style={styles.image} source={require('../assets/demo/TherapyScreenGif.gif')} />,
         title: 'Therapy session',
         subtitle: 'Read the scenarios and answer the questions. Click the text to speech button to read the scenario aloud.',
       },
       {
         backgroundColor: '#fff',
-        image: <Image resizeMode={'contain'} style={styles.image} source={require('../assets/demo/PauseScreen.png')} />,
+        image: <Image resizeMode={'contain'} style={styles.image} source={require('../assets/demo/PauseScreenGif.gif')} />,
         title: 'Take a break',
         subtitle: 'You can take a break in between your therapy questions by clicking the "Take a break" button at the top of your therapy screen.',
       },
       {
         backgroundColor: '#fff',
-        image: <Image resizeMode={'contain'} style={styles.image} source={require('../assets/demo/PlantScreen.png')} />,
+        image: <Image resizeMode={'contain'} style={styles.image} source={require('../assets/demo/PlantScreenGif.gif')} />,
         title: 'Water your plant and watch it grow!',
         subtitle: 'Water your plant with coins you have earned by logging your feelings and completing your therapy sessions. ',
       }
