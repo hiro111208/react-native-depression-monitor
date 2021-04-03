f9ec7aa8815419ca0b04bd4fb9cf1aace9208f40
Mordecai Sassoon <mordecai.sassoon@kcl.ac.uk>
2021-04-03 17:49:12 +0800

    Made userID first column of feelings csv file

--- app/dashboards/AdminHomeScreen.js
+++ app/dashboards/AdminHomeScreen.js
@@ -110,10 +110,10 @@ export default function AdminHomeScreen({ props, navigation }) {
   onCreateFeelingsCSV = async () => {
     setLoaded(true);
     firebase.firestore().collection("feelings").get().then(async (querySnapshot) => {
-      const headerString = 'anxious, friendly, overall, paranoid, sad, timeStamp, userId \n';
+      const headerString = 'userId, anxious, friendly, overall, paranoid, sad, timeStamp \n';
       let rowString = ''
       querySnapshot.forEach((doc) => {
-        rowString = rowString + `${doc.data().anxious}, ${doc.data().friendly}, ${doc.data().overall}, ${doc.data().paranoid}, ${doc.data().sad}, ${doc.data().timeStamp.toDate()}, ${doc.data().userID} \n`;
+        rowString = rowString + ` ${doc.data().userID}, ${doc.data().anxious}, ${doc.data().friendly}, ${doc.data().overall}, ${doc.data().paranoid}, ${doc.data().sad}, ${doc.data().timeStamp.toDate()} \n`;
       });
       const csvString = `${headerString}${rowString}`;
 
