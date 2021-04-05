1a77ad9ed85e0326b0f19d77dec1b9e897926a5f
Shehzad Haleem <shehzad.haleem@kcl.ac.uk>
2021-04-05 10:17:15 +0100

    fix whitespace and comments in AdminDashboard.js

--- app/dashboards/AdminDashboard.js
+++ app/dashboards/AdminDashboard.js
@@ -1,57 +1,63 @@
-import * as React from 'react';
-import { getFocusedRouteNameFromRoute } from '@react-navigation/native';
-import { Ionicons } from '@expo/vector-icons';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import * as React from "react";
+import { getFocusedRouteNameFromRoute } from "@react-navigation/native";
+import { Ionicons } from "@expo/vector-icons";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-import AdminHomeScreen from './AdminHomeScreen';
-import AdminUsersScreen from './AdminUsersScreen';
-
+import AdminHomeScreen from "./AdminHomeScreen";
+import AdminUsersScreen from "./AdminUsersScreen";
 
 const Tab = createBottomTabNavigator();
 
 function getHeaderTitle(route) {
-    // If the focused route is not found, we need to assume it's the initial screen
-    // This can happen during if there hasn't been any navigation inside the screen
-    // In our case, it's "Home" as that's the first screen inside the navigator
-    const routeName = getFocusedRouteNameFromRoute(route) ?? 'Home';
+  // If the focused route is not found, we need to assume it's the initial screen
+  // This can happen during if there hasn't been any navigation inside the screen
+  // In our case, it's "Home" as that's the first screen inside the navigator
+  const routeName = getFocusedRouteNameFromRoute(route) ?? "Home";
 
-    switch (routeName) {
-        case 'Home':
-            return 'Home';
-        case 'Users':
-            return 'Users';
-    }
+  switch (routeName) {
+    case "Home":
+      return "Home";
+    case "Users":
+      return "Users";
+  }
 }
 
-export default function AdminDashboard({ navigation, route}) {
-    React.useLayoutEffect(() => {
-        navigation.setOptions({ headerTitle: getHeaderTitle(route) });
-    }, [navigation, route]);
+export default function AdminDashboard({ navigation, route }) {
+  React.useLayoutEffect(() => {
+    navigation.setOptions({ headerTitle: getHeaderTitle(route) });
+  }, [navigation, route]);
 
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
 
-    return (
-            <Tab.Navigator
-                screenOptions={({ route }) => ({
-                    tabBarIcon: ({ focused, color, size }) => {
-                        let iconName;
+          if (route.name === "Home") {
+            iconName = focused ? "ios-home" : "ios-home-outline";
+          } else if (route.name === "Users") {
+            iconName = focused ? "ios-people" : "ios-people-outline";
+          }
 
-                        if (route.name === 'Home') {
-                            iconName = focused ? 'ios-home' : 'ios-home-outline';
-                        } else if (route.name === 'Users') {
-                            iconName = focused ? 'ios-people' : 'ios-people-outline';
-                        }
-
-                        // You can return any component that you like here!
-                        return <Ionicons name={iconName} size={size} color={color} />;
-                    },
-                })}
-                tabBarOptions={{
-                    activeTintColor: 'tomato',
-                    inactiveTintColor: 'gray',
-                }}
-            >
-                <Tab.Screen name="Home" component={AdminHomeScreen} options={{ tabBarLabel: 'Home' }} />
-                <Tab.Screen name="Users" component={AdminUsersScreen} options={{ tabBarLabel: 'Users' }} />
-            </Tab.Navigator>
-    );
+          // You can return any component that you like here!
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+      })}
+      tabBarOptions={{
+        activeTintColor: "tomato",
+        inactiveTintColor: "gray",
+      }}
+    >
+      <Tab.Screen
+        name="Home"
+        component={AdminHomeScreen}
+        options={{ tabBarLabel: "Home" }}
+      />
+      <Tab.Screen
+        name="Users"
+        component={AdminUsersScreen}
+        options={{ tabBarLabel: "Users" }}
+      />
+    </Tab.Navigator>
+  );
 }
