b78cb513e22df49d14369334bd902d46e62a910b
Shehzad Haleem <shehzad.haleem@kcl.ac.uk>
2021-04-06 10:53:36 +0100

    add comments to AdminDashboard.js

--- app/dashboards/AdminDashboard.js
+++ app/dashboards/AdminDashboard.js
@@ -6,9 +6,6 @@ import AdminUsersScreen from "./AdminUsersScreen";
 
 const Tab = createBottomTabNavigator();
 function getHeaderTitle(route) {
-  // If the focused route is not found, we need to assume it's the initial screen
-  // This can happen during if there hasn't been any navigation inside the screen
-  // In our case, it's 'Home' as that's the first screen inside the navigator
   const routeName = getFocusedRouteNameFromRoute(route) ?? "Home";
 
   switch (routeName) {
