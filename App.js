62400bdc981b7c764ef109fba694d2572540de02
Hiro Funatsuka <hiro.funatsuka@gmail.com>
2024-11-03 18:38:04 +0000

    Update files.

--- App.js
+++ App.js
@@ -2,8 +2,8 @@ import * as React from "react";
 import { View, Text, Button, Image, alert } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import {
-  createStackNavigator,
-  HeaderBackButton,
+    createStackNavigator,
+    HeaderBackButton,
 } from "@react-navigation/stack";
 import TherapyScreen from "./app/screens/TherapyScreen";
 import CategoryDrop from "./app/screens/CategoryDrop";
@@ -29,136 +29,139 @@ import { withNavigation } from "react-navigation";
 import { LogBox } from "react-native";
 
 LogBox.ignoreLogs([
-  "Non-serializable values were found in the navigation state",
-  "Warning: Can't perform a React state update on an unmounted component. This is a no-op, but it indicates a memory leak in your application. To fix, cancel all subscriptions and asynchronous tasks in a useEffect cleanup function.",
+    "Non-serializable values were found in the navigation state",
+    "Warning: Can't perform a React state update on an unmounted component. This is a no-op, but it indicates a memory leak in your application. To fix, cancel all subscriptions and asynchronous tasks in a useEffect cleanup function.",
 ]);
 
 function MyStack() {
-  const Stack = createStackNavigator();
-  return (
-    <Stack.Navigator
-      initialRouteName="LoginScreen"
-      screenOptions={{
-        gestureEnabled: false,
-        headerLeft: null,
-        headerTitleAlign: "center",
-        headerStyle: {
-          backgroundColor: colors.darkBorder,
-          borderBottomLeftRadius: 50,
-          borderBottomRightRadius: 50,
-          shadowColor: "#000",
-          shadowOffset: {
-            width: 0,
-            height: 2,
-          },
-          shadowOpacity: 0.25,
-          shadowRadius: 3.84,
-          elevation: 5,
-        },
-        headerTintColor: "white",
-        headerTitleStyle: {
-          fontWeight: "bold",
-        },
-      }}
-    >
-      <Stack.Screen
-        name="SignupScreen"
-        component={SignupScreen}
-        options={{ title: "SignUp" }}
-      />
+    const Stack = createStackNavigator();
+    return (
+        <Stack.Navigator
+            initialRouteName="LoginScreen"
+            screenOptions={{
+                gestureEnabled: false,
+                headerLeft: null,
+                headerTitleAlign: "center",
+                headerStyle: {
+                    backgroundColor: colors.darkBorder,
+                    borderBottomLeftRadius: 50,
+                    borderBottomRightRadius: 50,
+                    shadowColor: "#000",
+                    shadowOffset: {
+                        width: 0,
+                        height: 2,
+                    },
+                    shadowOpacity: 0.25,
+                    shadowRadius: 3.84,
+                    elevation: 5,
+                },
+                headerTintColor: "white",
+                headerTitleStyle: {
+                    fontWeight: "bold",
+                },
+            }}
+        >
+            <Stack.Screen
+                name="SignupScreen"
+                component={SignupScreen}
+                options={{ title: "SignUp" }}
+            />
 
-      <Stack.Screen
-        name="LoginScreen"
-        component={LoginScreen}
-        options={{ title: "Login" }}
-      />
+            <Stack.Screen
+                name="LoginScreen"
+                component={LoginScreen}
+                options={{ title: "Login" }}
+            />
 
-      <Stack.Screen
-        name="ForgotPasswordScreen"
-        component={ForgotPasswordScreen}
-        options={{ title: "Forgot Password" }}
-      />
-      <Stack.Screen
-        options={{ headerShown: false }}
-        name="DemoScreen"
-        component={DemoScreen}
-      />
+            <Stack.Screen
+                name="ForgotPasswordScreen"
+                component={ForgotPasswordScreen}
+                options={{ title: "Forgot Password" }}
+            />
+            <Stack.Screen
+                options={{ headerShown: false }}
+                name="DemoScreen"
+                component={DemoScreen}
+            />
 
-      <Stack.Screen name="PatientDashboard" component={PatientDashboard} />
+            <Stack.Screen
+                name="PatientDashboard"
+                component={PatientDashboard}
+            />
 
-      <Stack.Screen
-        name="AdminDashboard"
-        component={AdminDashboard}
-        options={{ title: "AdminDashboard" }}
-      />
+            <Stack.Screen
+                name="AdminDashboard"
+                component={AdminDashboard}
+                options={{ title: "AdminDashboard" }}
+            />
 
-      <Stack.Screen
-        name="SchedulingScreen"
-        component={SchedulingScreen}
-        options={{
-          title: "Add Schedule",
-          headerLeft: null,
-        }}
-      />
+            <Stack.Screen
+                name="SchedulingScreen"
+                component={SchedulingScreen}
+                options={{
+                    title: "Add Schedule",
+                    headerLeft: null,
+                }}
+            />
 
-      <Stack.Screen
-        name="ScheduleListScreen"
-        component={ScheduleListScreen}
-        options={{ title: "Schedule List" }}
-      />
+            <Stack.Screen
+                name="ScheduleListScreen"
+                component={ScheduleListScreen}
+                options={{ title: "Schedule List" }}
+            />
 
-      <Stack.Screen
-        options={{ headerShown: false }}
-        name="TherapyScreen"
-        component={TherapyScreen}
-      />
+            <Stack.Screen
+                options={{ headerShown: false }}
+                name="TherapyScreen"
+                component={TherapyScreen}
+            />
 
-      <Stack.Screen
-        name="PauseScreen"
-        component={PauseScreen}
-        options={{ headerShown: false }}
-      />
-      <Stack.Screen
-        options={{ headerShown: false }}
-        name="PlantScreen"
-        component={PlantScreen}
-      />
+            <Stack.Screen
+                name="PauseScreen"
+                component={PauseScreen}
+                options={{ headerShown: false }}
+            />
+            <Stack.Screen
+                options={{ headerShown: false }}
+                name="PlantScreen"
+                component={PlantScreen}
+            />
 
-      <Stack.Screen
-        name="LogFeelingScreen"
-        component={LogFeelingScreen}
-        options={{ headerShown: false }}
-      />
+            <Stack.Screen
+                name="LogFeelingScreen"
+                component={LogFeelingScreen}
+                options={{ headerShown: false }}
+            />
 
-      <Stack.Screen name="CategoryDrop" component={CategoryDrop} />
+            <Stack.Screen name="CategoryDrop" component={CategoryDrop} />
 
-      <Stack.Screen
-        name="TherapyQuestionScreen"
-        component={TherapyQuestionScreen}
-        options={{ title: "Therapy Questions" }}
-      />
+            <Stack.Screen
+                name="TherapyQuestionScreen"
+                component={TherapyQuestionScreen}
+                options={{ title: "Therapy Questions" }}
+            />
 
-      <Stack.Screen
-        name="TherapyQuestionDetailScreen"
-        component={TherapyQuestionDetailScreen}
-        options={{ title: "Question Detail" }}
-      />
+            <Stack.Screen
+                name="TherapyQuestionDetailScreen"
+                component={TherapyQuestionDetailScreen}
+                options={{ title: "Question Detail" }}
+            />
 
-      <Stack.Screen
-        options={{ headerShown: false }}
-        name="SupportResources"
-        component={SupportResources}
-      />
+            <Stack.Screen
+                options={{ headerShown: false }}
+                name="SupportResources"
+                component={SupportResources}
+            />
 
-      <Stack.Screen name="AccountScreen" component={AccountScreen} />
-    </Stack.Navigator>
-  );
+            <Stack.Screen name="AccountScreen" component={AccountScreen} />
+        </Stack.Navigator>
+    );
 }
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <MyStack />
-    </NavigationContainer>
-  );
+    return (
+        <NavigationContainer>
+            <MyStack />
+        </NavigationContainer>
+    );
 }
--- setupTests.js
+++ setupTests.js
@@ -1,3 +1,30 @@
 import { configure } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 configure({ adapter: new Adapter() });
+
+import "react-native-gesture-handler/jestSetup";
+import "./node_modules/@unimodules/react-native-adapter";
+import "./node_modules/@unimodules/core/src/AdapterProxy";
+import "./node_modules/expo-notifications";
+
+//*https://blog.codemagic.io/testing-react-native-apps-with-jest/
+jest.mock("react-native-reanimated", () => {
+    const Reanimated = require("node_modules/react-native-reanimated/mock");
+    Reanimated.default.call = () => {};
+    return Reanimated;
+});
+
+jest.mock("expo-notifications", () => {
+    return {
+        addEventListener: jest.fn(),
+        requestPermissions: jest.fn(() => Promise.resolve()),
+        getInitialNotification: jest.fn(() => Promise.resolve()),
+    };
+});
+
+// Silence the warning: Animated: `useNativeDriver` is not supported because the native animated module is missing
+jest.mock("react-native/Libraries/Animated/src/NativeAnimatedHelper");
+
+jest.mock("@unimodules/react-native-adapter");
+
+jest.mock("@unimodules/core/src/AdapterProxy");
