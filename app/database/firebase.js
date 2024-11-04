ddf8bdab15943ef772f5544540b9451b2d7cd0c1
Hiro Funatsuka <hiro.funatsuka@gmail.com>
2024-11-04 18:44:02 +0000

    Update dabase folder.

--- app/database/firebase.js
+++ app/database/firebase.js
@@ -3,13 +3,13 @@ import firebase from "firebase";
 require("firebase/auth");
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDwOUkdxC7kNNILoFTSx05PTUxRci_kcaw",
-  authDomain: "fireship-tutorial-646fc.firebaseapp.com",
-  projectId: "fireship-tutorial-646fc",
-  storageBucket: "fireship-tutorial-646fc.appspot.com",
-  messagingSenderId: "289064122528",
-  appId: "1:289064122528:web:ee39abfa2183c8499071c5",
-  measurementId: "G-D35RDH9V0E",
+    apiKey: "AIzaSyDwOUkdxC7kNNILoFTSx05PTUxRci_kcaw",
+    authDomain: "fireship-tutorial-646fc.firebaseapp.com",
+    projectId: "fireship-tutorial-646fc",
+    storageBucket: "fireship-tutorial-646fc.appspot.com",
+    messagingSenderId: "289064122528",
+    appId: "1:289064122528:web:ee39abfa2183c8499071c5",
+    measurementId: "G-D35RDH9V0E",
 };
 
 firebase.initializeApp(firebaseConfig);
