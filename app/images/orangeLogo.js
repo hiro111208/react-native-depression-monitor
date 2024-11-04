ccf5d42fbee400077c0fe43bb26e580913bfa164
Hiro Funatsuka <hiro.funatsuka@gmail.com>
2024-11-04 18:44:51 +0000

    Update images folder.

--- app/images/orangeLogo.js
+++ app/images/orangeLogo.js
@@ -1,9 +1,9 @@
 import React from "react";
 
 function orangeLogo() {
-  return (
-    <Image width={200} height={200} source={require("./OrangeLogo.jpeg")} />
-  );
+    return (
+        <Image width={200} height={200} source={require("./OrangeLogo.jpeg")} />
+    );
 }
 
 export default orangeLogo;
